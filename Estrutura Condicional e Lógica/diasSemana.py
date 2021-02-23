8a9b08742606dd6b1ce00b6395a32b2de3226bdb
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-02-23 19:48:26 -0300

    Dias da semana

---
+++ Estrutura Condicional e Lógica/diasSemana.py
@@ -0,0 +1,17 @@
+num = int (input())
+if(num == 1):
+    print ("Domingo")
+elif(num == 2):
+    print ("Segunda")
+elif(num == 3):
+    print ("Terca")
+elif(num == 4):
+    print ("Quarta")
+elif(num == 5):
+    print ("Quinta")
+elif(num == 6):
+    print ("Sexta")
+elif(num == 7):
+    print ("Sabado")
+else:
+    print ("valor invalido")
