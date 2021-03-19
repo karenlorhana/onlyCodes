cc9e5f86ad854cab9f8d8620428eafb47effa699
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-03-19 11:51:19 -0300

    Divisão por 7

---
+++ Estrutura de Repetição (For + While)/divisaoPorSete.py
@@ -0,0 +1,12 @@
+''' 
+    Developed by Karen Lorhana
+'''
+
+numero = int(input())
+
+while numero != 0:
+    if numero % 7 == 0:
+        print("S")
+    else:
+        print("N")
+    numero = int(input())
