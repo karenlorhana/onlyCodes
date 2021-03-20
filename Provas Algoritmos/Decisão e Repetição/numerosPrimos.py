e6f151ff1a04bef3dfcfedde54f50ed6e7753242
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-03-19 21:40:14 -0300

    Desafio dos números primos 2

---
+++ Provas Algoritmos/Decisão e Repetição/numerosPrimos.py
@@ -0,0 +1,21 @@
+num1 = int(input())
+num2 = int(input())
+soma = 0
+
+if num2 <= num1:
+    print("Digite um valor maior que o primeiro!")
+    num2 = int(input())
+    while num2 <= num1:
+        print("Digite um valor maior que o primeiro!")
+        num2 = int(input())
+for i in range(num1,num2+1):
+    qtdDivisoresPrimos = 0
+    for x in range(1,i+1):
+        if i % x == 0:
+            qtdDivisoresPrimos += 1
+    if qtdDivisoresPrimos == 2:
+        soma += i
+if soma != 0:
+    print(soma)
+else:
+    print("Sem números primos no intervalo informado.")
