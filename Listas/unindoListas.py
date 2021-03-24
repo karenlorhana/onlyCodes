8a8d30729fec5e805f5222c8ed65d4d5e2f50b69
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-03-24 14:25:09 -0300

    Unindo listas

---
+++ Listas/unindoListas.py
@@ -0,0 +1,26 @@
+l1 =[]
+l2 =[]
+qtdLista1 = int(input())
+if qtdLista1  == 0:
+    print("Erro - A lista deve ter pelo menos 1 elemento.")
+else:
+    
+    for i in range(1, qtdLista1+1):
+        lista1 = int(input())
+        l1.append(lista1)
+
+    qtdLista2 = int(input())
+    if qtdLista2 == 0:
+        print("Erro - A lista deve ter pelo menos 1 elemento.")
+    else:
+        for i in range(1, qtdLista2+1):
+            lista2 = int(input())
+            l2.append(lista2)
+        if((len(l1) == 0 or len(l2) == 0)):
+            print("Erro - A lista deve ter pelo menos 1 elemento.")
+        else:
+            listasUnidas = str(l1 + l2)
+            listasUnidas = listasUnidas.replace(",","")
+            listasUnidas = listasUnidas.strip("[")
+            listasUnidas = listasUnidas.strip("]")
+            print(listasUnidas)
