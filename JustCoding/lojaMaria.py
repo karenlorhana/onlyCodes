e8d61f4ab955190b312db6dd2f24e339e89472b3
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-04-20 15:50:59 -0300

    Update

---
+++ JustCoding/lojaMaria.py
@@ -0,0 +1,24 @@
+'''
+    developed by Karen Lorhana
+'''
+
+qtdDoces = int(input("Informe a quantidade de doces comprados: "))
+doces = 0
+preco = 0
+indice = 0
+maisBarato = 10000000
+
+for i in range(1,qtdDoces+1):
+    print("Doce ", i)
+    pesoDoces = int(input("Peso (g): "))
+    doces += pesoDoces
+    valorDoces = float(input("Valor (R$): "))
+    preco += valorDoces
+    precoUnitario = (valorDoces/pesoDoces) * 1000
+    print("Preço unitário: ", " R$", precoUnitario,"/kg")
+    if precoUnitario < maisBarato:
+        maisBarato = precoUnitario
+        indice = i
+    #Produto mais barato: Doce 2, R$21.02/kg
+print("Produto mais barato: Doce ",indice, ", R$", maisBarato,"/kg")
+print("Foram comprados ",doces, "g de doce por R$", preco)
