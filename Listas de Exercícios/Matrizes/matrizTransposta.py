47e3d4d4bd386c7c8c93fd5411be8ad9cdb92951
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-04-27 17:31:42 -0300

    Matriz transposta (The Huxley)

--- Listas de Exercícios/Matrizes/matrizTransposta.py
+++ Listas de Exercícios/Matrizes/matrizTransposta.py
@@ -1,10 +1,22 @@
-matriz = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-for lin in range(3):
-    for col in range(4):
-        matriz[lin][col] = int(input())
-matrizTransposta = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for transposta in range(4):
-    matrizTransposta[transposta][0] = matriz[0][transposta]
-    matrizTransposta[transposta][1] = matriz[1][transposta]
-    matrizTransposta[transposta][2] = matriz[2][transposta]
-print("A matriz transposta é: ", matrizTransposta)
+matrizUm = []
+matrizDois = []
+
+qtd = input().split()
+qtdLinha = int(qtd[0])
+qtdColuna = int(qtd[1])
+
+for i in range(qtdLinha):
+    linha = input().split()
+    linha = list(map(int,linha))
+    matrizUm.append(linha)
+
+for i in range(qtdColuna):
+    linha = []
+    for j in range(qtdLinha):
+        linha.append(matriz[j][i])
+    matrizDois.append(linha)
+
+for i in range(qtdColuna):
+    for j in range(qtdLinha):
+        print(matriz2[i][j], end = ' ')
+    print()
