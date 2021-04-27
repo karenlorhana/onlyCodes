8ac1b6829ec458525419f9a3f856be9afb2dd427
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-04-27 17:32:40 -0300

    Matriz fantasma (The Huxley)

---
+++ Listas de Exercícios/Matrizes/#matrizFantasma.py
@@ -0,0 +1,23 @@
+# ----------------------
+
+matriz = []
+n = int(input())
+
+for i in range(n):
+    cont = 1
+    linha = []
+    for j in range(n):
+        linha.append(cont)
+        cont += 1
+    matriz.append(linha)
+
+cont =-1 
+for i in range(n):
+    for j in range(n):
+        if j == n-1 and j>cont:
+            print(matriz[i][j])
+        elif j > cont:
+            print(matriz[i][j], end=' ')
+        else:
+            print("  ", end=' ')
+    cont +=1
