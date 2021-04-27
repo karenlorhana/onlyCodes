c4ad52960ddddbbe43ffcbbf2df1fe7fba848fb8
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-04-27 18:47:40 -0300

    Questão 02

---
+++ Listas de Exercícios/Matrizes/Lista/somaVizinhos.py
@@ -0,0 +1,21 @@
+matriz =    [[ 1, 2, 3],
+            [8, 10, 12],
+            [21, 24, 27]]
+soma = 0
+
+i = int(input())
+j = int(input())
+
+if i <=len(matriz) and j <=len(matriz[0]):
+    soma += matriz[i][j]
+    if j - 1 >= 0:
+        soma += matriz[i][j-1]
+    if j + 1 < len(matriz[i]):
+        soma += matriz[i][j+1]
+    if i - 1 >= 0:
+        soma += matriz[i-1][j]
+    if i + 1 < 0:
+        soma += matriz[i+1][j]
+    print(soma)
+else:
+    print("Digite posições válidas")
