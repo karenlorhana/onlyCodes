4354d513ac1f724499a2fd9847165bdf1f501bb3
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-04-28 14:30:59 -0300

    Matriz triangular superior (Questão 07)

---
+++ Listas de Exercícios/Matrizes/Lista/#triangularSuperior.py
@@ -0,0 +1,25 @@
+#-------------
+
+ 
+matriz = []
+
+for i in range(3):
+    linha = input().split()
+    linha = [int(x) for x in linha]
+    matriz.append(linha)
+
+achou_nao_zero = False
+
+for i in range(1, len(matriz)):
+    for j in range(0, i):
+        if matriz[i][j] != 0:
+            achou_nao_zero = True
+            break
+
+    if achou_nao_zero:
+        break
+
+if achou_nao_zero:
+  print("A matriz não é uma matriz triangular superior.")
+else:
+  print("A matriz é uma matriz triangular superior.")
