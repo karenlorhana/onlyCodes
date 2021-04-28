f3a65a8a48fbc412372bb859af38039e694500a6
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-04-28 11:57:39 -0300

    Update

--- Listas de Exercícios/Matrizes/exemplos.py
+++ Listas de Exercícios/Matrizes/exemplos.py
@@ -1,11 +1,13 @@
 #forma 1 de se declarar uma matriz
 
-matriz = [[0,0,0],[0,0,0],[0,0,0]    ]
+matriz = [[0,0,0],[0,0,0],[0,0,0]  ]
 for lin in range(0, 3):
     for col in range(0, 3):
         matriz[lin][col] = int(input(f"digite um valor para [{lin},{col}] :"))
 print(matriz)
+
 print("--"*20)
+print()
 
 #forma 2 de se declarar uma matriz
 matriz2 = []
@@ -17,12 +19,17 @@ for lin in range(0, 3):
     matriz2.append(lista)
 print(matriz2)
 
+print("--"*20)
+print()
+
 #imprimir matriz
 for lin in range(0,3):
     for col in range(0,3):
         print(matriz[lin][col], end=' ')
     print()
 
+print("--"*20)
+print()
 
 #imprimir matriz
 
@@ -33,3 +40,17 @@ for i in range(qtd):
         else:
             print(matriz[i][j], end = ' ')
 
+print("--"*20)
+print()
+
+#percorrer diagonal principal
+
+for i in range(3): 
+   print(matriz[i][i], end=" ")
+
+print("--"*20)
+print()
+
+#percorrer diagonal secundária
+for i in range(3):
+        somaSecundaria += matriz[i][-1 -i]
