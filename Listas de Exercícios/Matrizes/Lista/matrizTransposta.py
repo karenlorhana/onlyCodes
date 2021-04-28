1ad274e88dc1ff6c9db17122bfd7494ee0b4a636
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-04-28 14:31:23 -0300

    Matriz transposta (Questão 05)

---
+++ Listas de Exercícios/Matrizes/Lista/matrizTransposta.py
@@ -0,0 +1,22 @@
+matrizUm = []
+matrizDois = []
+
+qtd = input().split()
+qtdLinha = int(qtd[0])
+qtdColuna = int(qtd[1])
+
+for i in range(qtdLinha):
+    linha = input().split()
+    linha = list(map(int,linha))
+    matrizUm.append(linha)
+
+for i in range(qtdColuna):
+    linha = []
+    for j in range(qtdLinha):
+        linha.append(matrizUm[j][i])
+    matrizDois.append(linha)
+
+for i in range(qtdColuna):
+    for j in range(qtdLinha):
+        print(matrizDois[i][j], end = ' ')
+    print()
